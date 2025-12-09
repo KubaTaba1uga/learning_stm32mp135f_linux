ad099a7c242a8099130b8617e244c67b9bb10c5f
KubaTaba1uga <luna7337anul@proton.me>
2025-12-09 19:22:52 +0100

    WIP

--- examples/btn_lkm_interrupt/app/main.c
+++ examples/btn_lkm_interrupt/app/main.c
@@ -1,5 +1,7 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -11,34 +13,41 @@ struct input_event {
   unsigned int value;
 };
 
-int main(void) {
-  int fd;
+int main(int argc, char *argv[]) {
+  char *input_dev = "/dev/input/event0";
+  if (argc > 1) {
+    input_dev = argv[1];
+  }
 
-  fd = open("/dev/input/event0", O_RDONLY);
+  int fd;
+  fd = open(input_dev, O_RDONLY);
   if (fd == -1) {
     perror("Unable to open");
     return EXIT_FAILURE;
   }
 
-  struct input_event event;
-  ssize_t retval = read(fd, &event, sizeof(event));
+  while (1) {
+    struct input_event event;
+    ssize_t retval = read(fd, &event, sizeof(event));
 
-  if (retval == -1) {
-    perror("read()");
-    return EXIT_FAILURE;
-  } else if (retval != sizeof(event)) {
-    perror("read()");
-    fprintf(stderr, "Short read: got %zd (not %zd) bytes\n", retval, sizeof(event));
-    return EXIT_FAILURE;
+    if (retval == -1) {
+      perror("read()");
+      return EXIT_FAILURE;
+    } else if (retval != sizeof(event)) {
+      perror("read()");
+      fprintf(stderr, "Short read: got %zd (not %zd) bytes\n", retval,
+              sizeof(event));
+      return EXIT_FAILURE;
+    }
+
+    printf("input_event {\n");
+    printf("  time:  %ld.%06ld\n", (long)event.time.tv_sec,
+           (long)event.time.tv_usec);
+    printf("  type:  0x%04hx (%hu)\n", event.type, event.type);
+    printf("  code:  0x%04hx (%hu)\n", event.code, event.code);
+    printf("  value: 0x%08x (%u)\n", event.value, event.value);
+    printf("}\n");
   }
-
-  printf("input_event {\n");
-  printf("  time:  %ld.%06ld\n",
-         (long)event.time.tv_sec, (long)event.time.tv_usec);
-  printf("  type:  0x%04hx (%hu)\n", event.type, event.type);
-  printf("  code:  0x%04hx (%hu)\n", event.code, event.code);
-  printf("  value: 0x%08x (%u)\n", event.value, event.value);
-  printf("}\n");
 
   close(fd);
   return EXIT_SUCCESS;
